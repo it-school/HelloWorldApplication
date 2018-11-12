824d09bf248b87d68c5fdb16e7d9e195c0f887a0
it-school <it@osvita.od.ua>
2018-11-12 20:41:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        int operand1 = 0;
+        int operand2 = 3;
+        int sum;
+        final double PI = 3.14;
+
+        byte a = 127;
+        a = (byte)(a + 1);
+        System.out.println(a);
+     
+/*        operand1 = 2;
+        sum = operand1 + operand2;
+        System.out.println(sum);
+*/
+    }
+}
